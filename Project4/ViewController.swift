28431f33917f6fb4648fe6842733377e74b0410f
Saurabh <saurabh.agarwal@mxplayer.in>
2022-09-26 13:26:49 +0530

    learning WKWebView

--- Project4/ViewController.swift
+++ Project4/ViewController.swift
@@ -1,3 +1,4 @@
+
 //
 //  ViewController.swift
 //  Project4
@@ -6,12 +7,26 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+//for classes the first Class after the : is the class it extends from and the subsequent classes are those whose protocols the current class conforms to, i.e, it takes care of the methods in those class, this helps in delegation
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
+    
+    override func loadView() {
+         webView = WKWebView()
+        webView.navigationDelegate = self
+        //this above line is type of programming pattern and is called "delegation", a delegate is one thing acting in place of another
+        //this above means that whenever any webpage navigation happens in our app it should notify the current view controller
+        view = webView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let url = URL(string: "https://www.hackingwithswift.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
     }
 
 
